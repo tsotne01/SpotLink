ff8572dd579069b1c8a2a05282deed3a1bad39b5
tsotne01 <tsotnechkhenkeli@gmail.com>
2025-04-06 18:13:02 +0400

    feat: feat/SPOT-17-fe-implement-email-password-input-validation added validation using react-hook-form and zod on registration and signin pages.

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import Input from '../Input/Input'
+import { useForm } from 'react-hook-form'
+import z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const singinSchema = z.object({
+    email: z.string().email().min(1),
+    password: z.string().min(8)
+})
 
 function SignIn() {
+    const { register, handleSubmit, formState: { errors, isLoading } } = useForm({
+        resolver: zodResolver(singinSchema)
+    });
+    const handleSignIn = (data)=>{
+        console.log(data);
+    }
     return (
-        <form className="flex flex-col gap-5">
+        <form onSubmit={handleSubmit(handleSignIn)} className="flex flex-col gap-5">
             <div className="signup flex flex-col gap-y-5">
                 <div>
-                    <Input label="Input Email" type="email" id="email" placeholder="Enter your email." />
+                    <Input {...register("email")} id="email" type="email" label="Input Email"  placeholder="Enter your email." />
+                    {errors.email?.message && <p className='text-red-600'>{errors.email.message}</p>}
+
                 </div>
                 <div>
-                    <Input label="Input password" type="password" id="password" placeholder="Enter your password." />
+                    <Input {...register("password")} id="password" type="password" label="Input password" placeholder="Enter your password." />
+                    {errors.password?.message && <p className='text-red-600'>{errors.password.message}</p>}
                 </div>
 
             </div>
-            <button className="px-3 py-2 bg-slate-900 text-amber-50 rounded-sm cursor-pointer">
+            <button disabled={isLoading} className="px-3 py-2 bg-slate-900 text-amber-50 rounded-sm cursor-pointer">
                 Sign In
             </button>
         </form>
